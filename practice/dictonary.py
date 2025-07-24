4043fedebb83c6280db56f87e5de514a0c123765
aishu-v1717 <vydhamaishwarya@gmail.com>
2025-07-24 19:52:59 +0530

    dic

---
+++ practice/dictonary.py
@@ -0,0 +1,10 @@
+dic={}
+print(dic)
+dic={"name":"aishu","age":21}
+print(dic)
+print(dic["name"])  # accessing value by key
+print(dic.get("age"))  # using get method
+print(dic.keys())  # get all keys
+print(dic.values())  # get all values
+print(dic.items())  # get all key-value pairs
+print("name" in dic)  # check if key exists
